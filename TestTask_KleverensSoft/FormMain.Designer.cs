5da866c7eeaa3bc4fb2fc283f6df845bb6dcb8a8
Alex Vaisero <vaiserowork@gmail.com>
2024-02-02 06:59:19 +0300

    Visual update

--- TestTask_KleverensSoft/FormMain.Designer.cs
+++ TestTask_KleverensSoft/FormMain.Designer.cs
@@ -3,14 +3,14 @@
     partial class FormMain
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Освободить все используемые ресурсы.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,85 +20,127 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.Task1 = new System.Windows.Forms.Button();
-            this.Task2 = new System.Windows.Forms.Button();
-            this.Task3 = new System.Windows.Forms.Button();
+            this.textBoxOriginal = new System.Windows.Forms.TextBox();
+            this.textBoxCompressed = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ProcessingCompress = new System.Windows.Forms.Button();
+            this.CleanAll = new System.Windows.Forms.Button();
+            this.ProcessingDeCompress = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Task1
+            // textBoxOriginal
             // 
-            this.Task1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.Task1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Task1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.Task1.Location = new System.Drawing.Point(0, 0);
-            this.Task1.Name = "Task1";
-            this.Task1.Size = new System.Drawing.Size(1197, 97);
-            this.Task1.TabIndex = 0;
-            this.Task1.Text = "Задание  #1";
-            this.Task1.UseVisualStyleBackColor = false;
-            this.Task1.Click += new System.EventHandler(this.Task1_Click);
+            this.textBoxOriginal.Location = new System.Drawing.Point(397, 12);
+            this.textBoxOriginal.Multiline = true;
+            this.textBoxOriginal.Name = "textBoxOriginal";
+            this.textBoxOriginal.Size = new System.Drawing.Size(356, 157);
+            this.textBoxOriginal.TabIndex = 0;
             // 
-            // Task2
+            // textBoxCompressed
             // 
-            this.Task2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Task2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.Task2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Task2.Location = new System.Drawing.Point(0, 139);
-            this.Task2.Name = "Task2";
-            this.Task2.Size = new System.Drawing.Size(1197, 97);
-            this.Task2.TabIndex = 1;
-            this.Task2.Text = "Задание  #2";
-            this.Task2.UseVisualStyleBackColor = false;
-            this.Task2.Click += new System.EventHandler(this.Task2_Click);
+            this.textBoxCompressed.Location = new System.Drawing.Point(397, 281);
+            this.textBoxCompressed.Multiline = true;
+            this.textBoxCompressed.Name = "textBoxCompressed";
+            this.textBoxCompressed.Size = new System.Drawing.Size(356, 157);
+            this.textBoxCompressed.TabIndex = 1;
             // 
-            // Task3
+            // label1
             // 
-            this.Task3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.Task3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Task3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Task3.Location = new System.Drawing.Point(0, 282);
-            this.Task3.Name = "Task3";
-            this.Task3.Size = new System.Drawing.Size(1197, 97);
-            this.Task3.TabIndex = 2;
-            this.Task3.Text = "Задание  #3";
-            this.Task3.UseVisualStyleBackColor = false;
-            this.Task3.Click += new System.EventHandler(this.Task3_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(276, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Исходная строка    ---";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Обработанная строка  ---";
+            // 
+            // ProcessingCompress
+            // 
+            this.ProcessingCompress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ProcessingCompress.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProcessingCompress.Location = new System.Drawing.Point(397, 175);
+            this.ProcessingCompress.Name = "ProcessingCompress";
+            this.ProcessingCompress.Size = new System.Drawing.Size(107, 50);
+            this.ProcessingCompress.TabIndex = 4;
+            this.ProcessingCompress.Text = "Сделать Компрессию";
+            this.ProcessingCompress.UseVisualStyleBackColor = false;
+            this.ProcessingCompress.Click += new System.EventHandler(this.ProcessingCompress_Click);
+            // 
+            // CleanAll
+            // 
+            this.CleanAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.CleanAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CleanAll.Location = new System.Drawing.Point(521, 175);
+            this.CleanAll.Name = "CleanAll";
+            this.CleanAll.Size = new System.Drawing.Size(107, 50);
+            this.CleanAll.TabIndex = 5;
+            this.CleanAll.Text = "Очистить всё";
+            this.CleanAll.UseVisualStyleBackColor = false;
+            this.CleanAll.Click += new System.EventHandler(this.CleanAll_Click);
+            // 
+            // ProcessingDeCompress
+            // 
+            this.ProcessingDeCompress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ProcessingDeCompress.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProcessingDeCompress.Location = new System.Drawing.Point(646, 175);
+            this.ProcessingDeCompress.Name = "ProcessingDeCompress";
+            this.ProcessingDeCompress.Size = new System.Drawing.Size(107, 50);
+            this.ProcessingDeCompress.TabIndex = 6;
+            this.ProcessingDeCompress.Text = "Сделать Декомпрессию";
+            this.ProcessingDeCompress.UseVisualStyleBackColor = false;
+            this.ProcessingDeCompress.Click += new System.EventHandler(this.ProcessingDeCompress_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1197, 379);
-            this.Controls.Add(this.Task3);
-            this.Controls.Add(this.Task2);
-            this.Controls.Add(this.Task1);
+            this.BackgroundImage = global::TestTask_KleverensSoft.Properties.Resources.rose;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ProcessingDeCompress);
+            this.Controls.Add(this.CleanAll);
+            this.Controls.Add(this.ProcessingCompress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCompressed);
+            this.Controls.Add(this.textBoxOriginal);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormMain";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Тестовое задание компании ООО Клеверенс Софт";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Task1;
-        private System.Windows.Forms.Button Task2;
-        private System.Windows.Forms.Button Task3;
+        private System.Windows.Forms.TextBox textBoxOriginal;
+        private System.Windows.Forms.TextBox textBoxCompressed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ProcessingCompress;
+        private System.Windows.Forms.Button CleanAll;
+        private System.Windows.Forms.Button ProcessingDeCompress;
     }
 }
-
--- TestTask_KleverensSoft/FormMain.cs
+++ TestTask_KleverensSoft/FormMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace TestTask_KleverensSoft
@@ -8,24 +9,126 @@ namespace TestTask_KleverensSoft
         public FormMain()
         {
             InitializeComponent();
+            textBoxOriginal.Text = string.Empty;
+            textBoxCompressed.Text = string.Empty;
         }
 
-        private void Task1_Click(object sender, EventArgs e)
+        private void ProcessingCompress_Click(object sender, System.EventArgs e)
         {
-            var FormTask = new FormTask1();
-            FormTask.ShowDialog();
+            bool AreAllLettersSmallAndLatin = true;
+            if (textBoxOriginal.Text != string.Empty)
+            {
+                for (int i = 0; i < textBoxOriginal.Text.Length; i++)
+                {
+                    if (!Regex.IsMatch(textBoxOriginal.Text[i].ToString(), @"^[a-z]+$"))
+                        AreAllLettersSmallAndLatin = false;
+                }
+
+                if (AreAllLettersSmallAndLatin)
+                    Compress();
+                else MessageBoxError();
+            }
         }
 
-        private void Task2_Click(object sender, EventArgs e)
+        private void CleanAll_Click(object sender, System.EventArgs e)
         {
-            var FormTask = new FormTask2();
-            FormTask.ShowDialog();
+            textBoxOriginal.Text = string.Empty;
+            textBoxCompressed.Text = string.Empty;
         }
 
-        private void Task3_Click(object sender, EventArgs e)
+        private void ProcessingDeCompress_Click(object sender, System.EventArgs e)
         {
-            var FormTask = new FormTask3();
-            FormTask.ShowDialog();
+            bool AreAllLettersSmallAndLatin = true;
+            if (textBoxOriginal.Text != string.Empty)
+            {
+                for (int i = 0; i < textBoxOriginal.Text.Length; i++)
+                {
+                    if (!Regex.IsMatch(textBoxOriginal.Text[i].ToString(), @"^[a-z0-9]+$") ||
+                        char.IsDigit(textBoxOriginal.Text[0]))
+                        AreAllLettersSmallAndLatin = false;
+                }
+
+                if (AreAllLettersSmallAndLatin)
+                    Decompress();
+                else MessageBoxError();
+            }
         }
+
+
+
+
+
+
+
+
+
+        private void Compress()
+        {
+            StringBuilder compressed = new StringBuilder();
+            int count = 1;
+
+            if (textBoxOriginal.Text.Length < 2)
+                compressed.Append(textBoxOriginal.Text);
+            else
+                for (int i = 1; i <= textBoxOriginal.Text.Length; i++)
+                {
+                    if (i < textBoxOriginal.Text.Length && textBoxOriginal.Text[i] == textBoxOriginal.Text[i - 1])
+                        count++;
+
+                    else
+                    {
+                        compressed.Append(textBoxOriginal.Text[i - 1]);
+
+                        if (count > 1)
+                            compressed.Append(count);
+
+                        count = 1;
+                    }
+                }
+
+            textBoxCompressed.Text = compressed.ToString();
+        }
+
+        private void Decompress()
+        {
+            StringBuilder decompressed = new StringBuilder();
+            int i = 0;
+
+            if (textBoxOriginal.Text.Length < 2)
+                decompressed.Append(textBoxOriginal.Text);
+            else
+                while (i < textBoxOriginal.Text.Length)
+                {
+                    char currentChar = textBoxOriginal.Text[i];
+                    decompressed.Append(currentChar);
+
+                    if (i + 1 < textBoxOriginal.Text.Length && char.IsDigit(textBoxOriginal.Text[i + 1]))
+                    {
+                        int count = int.Parse(textBoxOriginal.Text[i + 1].ToString());
+
+                        for (int j = 0; j < count - 1; j++)
+                        {
+                            decompressed.Append(currentChar);
+                        }
+
+                        i++;
+                    }
+
+                    i++;
+                }
+
+            textBoxCompressed.Text = decompressed.ToString();
+        }
+
+        private void MessageBoxError()
+        {
+            MessageBox.Show(
+                "Неверные начальные данные!",
+                "Внимание, ошибка",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+        }
+
     }
 }
